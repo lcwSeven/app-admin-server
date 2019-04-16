39c6063b2314fd73682cc6292f81c3a3553a5251
seven <liucaiwenSeven@163.com>
2019-04-17 00:36:54 +0800

    import:单元测试

--- src/test/java/com/app/admin/server/ApplicationTest.java
+++ src/test/java/com/app/admin/server/ApplicationTest.java
@@ -1,9 +1,12 @@
 package com.app.admin.server;
 
+import com.app.admin.server.bean.AppDataManager;
+import com.app.admin.server.dao.AppDataManagerMapper;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -22,14 +25,46 @@ public class ApplicationTest {
         System.out.println("开始测试-----------------");
 
     }
+
     @Test
-    public void test(){
+    public void test() {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         System.out.println(encoder.encode("123456"));
     }
 
+    @Autowired
+    private AppDataManagerMapper appDataManagerMapper;
+    @Test
+    public void insertTest(){
+        for (int i = 0; i < 50; i++) {
+            AppDataManager appDataManager = new AppDataManager();
+            if (i%2==0){
+                appDataManager.setDataOne("我是数据"+i);
+            }else {
+                appDataManager.setDataOne("kkka"+i);
+            }
+            if (i%3==0){
+                appDataManager.setDataTwo("我是数据i%3"+i);
+            }else {
+                appDataManager.setDataTwo("llll%3"+i);
+            }
+            appDataManager.setDataThree("aldh");
+            appDataManager.setDataFour("data4");
+            appDataManager.setDataFive("datalllll5");
+            appDataManager.setDataSix("asgduad6");
+            appDataManager.setDataSeven("apppp7");
+            appDataManager.setDataEight(";ewjiwj");
+            appDataManager.setDataNine("eight888");
+            appDataManager.setDataTen("101000");
+
+            appDataManagerMapper.insertSelective(appDataManager);
+
+        }
+    }
+
 
     @After
+
     public void after() {
         System.out.println("测试结束-----------------");
 
---
+++ src/test/java/com/app/admin/server/service/impl/AppDataManagerTest.java
@@ -0,0 +1,8 @@
+package com.app.admin.server.service.impl;
+
+/**
+ * @author: liucaiwen
+ * @date: 2019/4/8 22:54
+ */
+public class AppDataManagerTest {
+}
